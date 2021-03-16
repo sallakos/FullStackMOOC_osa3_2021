0476931818643ee11c8562410c17c7bf31efd5cf
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-03-16 20:17:32 +0200

    part 3.4 add possibility to delete a person

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -35,6 +35,12 @@ app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find((person) => person.id === id)
   person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter((person) => person.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
